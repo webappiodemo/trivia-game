e8262a2290a80ad434865a52b83430a15ef6d6b0
Colin Chartier <colin@webapp.io>
2022-06-01 17:22:04 -0400

    Add a query to supabase

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { createClient } from '@supabase/supabase-js'
+import {useEffect, useState} from "react";
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient('https://iheesrzbuoyqqozhfxjp.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImloZWVzcnpidW95cXFvemhmeGpwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTQxMTcyNjUsImV4cCI6MTk2OTY5MzI2NX0.cuBZi_a6g-bNcVizyjnRe7pFWkao3XHp6UU-TVp-QZA')
+
 function App() {
+  const [players, setPlayers] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(async () => {
+    const {data, error} = await supabase.from("players").select("name");
+    if(error) {
+      setError(error);
+    } else {
+      setPlayers(data);
+    }
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Welcome to the trivia game!
+          Players are: {players.join(", ")}
         </p>
         <a
           className="App-link"
