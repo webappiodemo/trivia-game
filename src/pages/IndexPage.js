8b275db2b873ca60283ac25519a83bb79d459fb3
Colin Chartier <colin@webapp.io>
2022-06-01 21:03:35 -0400

    Main landing page

--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -2,9 +2,12 @@ import React from "react";
 import {Link} from "react-router-dom";
 
 export default function IndexPage() {
-    return <div>
-        <div>Index page!</div>
-        <Link to="/create">Create game</Link>
-        <Link to="/join">Join game</Link>
+    return <div className="container flex flex-col space-y-16 text-dark items-center content-center">
+        <h1 className="text-lg font-bold text-center">Alcoholympics</h1>
+        <p className="text-md font-bold text-center">Play trivia with your friends</p>
+        <div className="flex space-x-16">
+            <Link to="/create" className="rounded-lg border border-purple-700 bg-purple-500 text-white py-4 px-8">Create game</Link>
+            <Link to="/join" className="rounded-lg border border-purple-700 text-dark py-4 px-8">Join game</Link>
+        </div>
     </div>
 }
