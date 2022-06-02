5990caf528b45c51a8469d17fabeba9787fc6de9
Colin Chartier <colin@webapp.io>
2022-06-01 21:04:16 -0400

    Disable supabase for now

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import { createClient } from '@supabase/supabase-js'
+// import { createClient } from '@supabase/supabase-js'
 import {Route, BrowserRouter as Router, Routes, Navigate} from "react-router-dom";
 import LoginPage from "./pages/Login";
 import IndexPage from "./pages/IndexPage";
@@ -9,7 +9,7 @@ import JoinGamePage from "./pages/JoinGamePage";
 import GamePage from "./pages/GamePage";
 
 // Create a single supabase client for interacting with your database
-const supabase = createClient('https://iheesrzbuoyqqozhfxjp.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImloZWVzcnpidW95cXFvemhmeGpwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTQxMTcyNjUsImV4cCI6MTk2OTY5MzI2NX0.cuBZi_a6g-bNcVizyjnRe7pFWkao3XHp6UU-TVp-QZA')
+// const supabase = createClient('https://iheesrzbuoyqqozhfxjp.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImloZWVzcnpidW95cXFvemhmeGpwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTQxMTcyNjUsImV4cCI6MTk2OTY5MzI2NX0.cuBZi_a6g-bNcVizyjnRe7pFWkao3XHp6UU-TVp-QZA')
 
 function App() {
   return <Router>
